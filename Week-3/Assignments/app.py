8d6b01e498d029adc2c8ea1ec79200b9e77cc3b1
Angel chang <greengirl860423@gmail.com>
2023-07-15 17:40:47 +0800

    assignment-4 ver.1

--- Week-3/Assignments/app.py
+++ Week-3/Assignments/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response
 app = Flask(__name__)
 
 
@@ -23,6 +23,40 @@ def data():
 @app.route("/sum.html")
 def sum_page():
     return render_template("sum.html")
+
+
+@app.route("/myName")
+def name():
+    userName = request.cookies.get('userID')
+    if userName is None:
+        return set_cookie_html()
+    else:
+        return get_cookie_html()
+
+
+@app.route("/setcookie.html")
+def set_cookie_html():
+    return render_template("setcookie.html")
+
+
+@app.route("/setcookie", methods=['POST'])
+def setcookie():
+    if request.method == 'POST':
+        user = request.form['nm']
+        resp = make_response(render_template('getcookie.html'))
+        resp.set_cookie('userID', user)
+    return resp
+
+
+@app.route("/getcookie.html", methods=['GET'])
+def get_cookie_html():
+    return render_template('getcookie.html')
+
+
+@app.route("/getcookie", methods=['GET'])
+def getcookie():
+    userName = request.cookies.get('userID')
+    return f"<h1>{userName}</h1>"
 
 
 if __name__ == '__main__':
